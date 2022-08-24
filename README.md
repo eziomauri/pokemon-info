149c34a1c9d084b13f317285e40d64895ebe1b66
Ezio Mauri <ezioluca.mauri@gmail.com>
2022-08-24 13:39:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pokemon Info
+
+Project to create a simple pokemon lookup website to get quick infos based on the async await Javascript functionality and fetching data using the PokeApi API
---
+++ index.js
@@ -0,0 +1,47 @@
+// grab the things we need ----------
+const pokemonContainer = document.querySelector(".pokemon-container");
+const formEl = document.querySelector("form");
+const inputEl = document.querySelector("input[type=text]");
+
+// listen for user events -------------
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    pokemonContainer.innerHTML = "";
+    getPokemon(inputEl.value);
+});
+
+async function getPokemon(name = "bulbasaur") {
+    const res= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const pokemon = await res.json();
+    console.log(pokemon);
+
+    const pokemonEl = document.createElement("div");
+    pokemonEl.classList.add("pokemon");
+
+    pokemonEl.innerHTML = `
+      <div class="info">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" width="200">
+    <h2>${pokemon.name}</h2>
+      </div>
+      <div class="stats">
+        ${pokemon.stats.map((stat) => {
+            return `<p>${stat.stat.name}: ${stat.base_stat}</p>`;
+          }).join("")}
+      </div>
+      <div class="abilities">
+      ${pokemon.abilities
+        .map((ability) => {
+          return `<p>${ability.ability.name}</p>`;
+        }).join("")}
+      <div>
+      <div class="moves">
+      ${pokemon.moves.map((move) => {
+          return `${move.move.name}  `;
+        })
+        .join("")}
+      <div>
+    `;
+    pokemonContainer.appendChild(pokemonEl);
+}
+// run things ----------------
+getPokemon();
